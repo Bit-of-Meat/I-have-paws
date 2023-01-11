8d97e4cd0af74cdf4d75bb32a5daa80e0833661d
zhjckfd135 <122279615+zhjckfd135@users.noreply.github.com>
2023-01-11 23:16:17 +0300

    Пауза

--- Assets/Scripts/Volume.cs
+++ Assets/Scripts/Volume.cs
@@ -4,13 +4,22 @@ using UnityEngine.UI;
 public class Volume : MonoBehaviour
 {
     static public float volume = 0;
+    static private bool isSetVolume = false;
     [SerializeField] private AudioSource _audio;
     [SerializeField] private Slider _volumeSlider;
 
+    private void Start()
+    {
+        if(isSetVolume)
+        {
+            _volumeSlider.value = volume;
+        }
+        isSetVolume = true;
+    }
 
     void Update()
     {
-        if(_volumeSlider != null) volume = _volumeSlider.value;
-        _audio.volume = volume;
+        if (_volumeSlider != null) volume = _volumeSlider.value;
+        if (_audio != null) _audio.volume = volume;
     }
 }
